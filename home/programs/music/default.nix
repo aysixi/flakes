56bcca41c1c9a8ce58d6090c857ab79c4ec51cce
aysixi <78737594+aysixi@users.noreply.github.com>
2025-05-02 11:29:50 +0900

    u

--- home/programs/music/default.nix
+++ home/programs/music/default.nix
@@ -1,26 +1,53 @@
-{ pkgs, ... }:
-
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  imports = [
+    ./go-musicfox.nix
+  ];
   home = {
     packages = with pkgs; [
-      cava
       mpc-cli
-      go-musicfox
     ];
   };
   programs = {
     ncmpcpp = {
       enable = true;
       mpdMusicDir = null;
+      settings = {
+        mpd_music_dir = "~/Music";
+      };
+    };
+    cava = {
+      enable = true;
+      settings = {
+        color = {
+          gradient = 1;
+          gradient_count = 5;
+          gradient_color_1 = "'#81A1C1'";
+          gradient_color_2 = "'#8FBCBB'";
+          gradient_color_3 = "'#B48EAD'";
+          gradient_color_4 = "'#eceff4'";
+          gradient_color_5 = "'#EBCB8B'";
+        };
+      };
     };
   };
   home.file = {
-    ".config/ncmpcpp/config".text = ''
-      mpd_music_dir = ~/Music
-    '';
     # ".config/mpd/mpd.conf".text = import ./mpd.nix;
-    ".config/cava/config".source = ./cava_config;
-    ".config/cava/config_internal".source = ./cava_config_internal;
+    # ".config/cava/config".source = ./cava_config;
+    ".config/cava/config_internal".text = lib.mkIf (config.programs.cava.enable) ''
+      [general]
+      bars = 12
+      sleep_timer = 10
+      [output]
+      method = raw
+      data_format = ascii
+      ascii_max_range = 7
+    ''; # ../waybar/{share_scripts.nix}.config_internal;
   };
 
   services = {
