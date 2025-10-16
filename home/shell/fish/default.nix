d3bca1fc4829853f34c4f201a244e082a83ea100
aysixi <78737594+aysixi@users.noreply.github.com>
2025-10-16 19:15:16 +0900

    fix

--- home/shell/fish/default.nix
+++ home/shell/fish/default.nix
@@ -28,7 +28,7 @@
       nf = ''nvim (FZF_DEFAULT_COMMAND='fd' FZF_DEFAULT_OPTS="--preview 'bat --style=numbers --color=always --line-range :500 {}'" fzf --height 60% --layout reverse --info inline --border --color 'border:#b48ead')'';
       # r = "yazi";
       top = "btop";
-      grep = "grep --exclude-dir=.git";
+      grep = "grep --exclude-dir=.git --color=auto";
     };
 
     functions = {
